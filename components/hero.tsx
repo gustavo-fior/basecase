8f98a065d51365c0a952da85f4828c708d2f8f8d
alanagoyal <alanaan@gmail.com>
2024-11-02 18:13:32 -0700

    change hero colors

--- components/hero.tsx
+++ components/hero.tsx
@@ -3,12 +3,12 @@
 export const Hero = () => {
   return (
     <div className="max-w-3xl py-10">
-      <h1 className="text-4xl mb-6">
+      <h1 className="text-4xl mb-6 text-orange-500">
         A builder
         <br />
-        <span className="text-orange-500">backing builders</span>
+        <span>backing builders</span>
       </h1>
-      <p className="text-xl text-gray-400">
+      <p className="text-xl">
         Basecase invests in founders before their companies exist. We write the
         first check to technical founders who are often still in the dreaming,
         building, and exploring phase of their journey.
