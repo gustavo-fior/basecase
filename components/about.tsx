7e1512a283597fba636351a32411e1372af55cac
alanagoyal <alanaan@gmail.com>
2024-11-03 16:54:23 -0800

    i to we

--- components/about.tsx
+++ components/about.tsx
@@ -96,13 +96,13 @@ export const About = () => {
       </DisintegratingText>
       <div className="grid md:grid-cols-2 gap-12">
         <div className="space-y-4">
-          <h2 className="font-semibold text-lg">How I work</h2>
+          <h2 className="font-semibold text-lg">How We work</h2>
           {[
-            "I write the very first check into companies",
-            "I lead rounds with $1-2M checks and don't take board seats",
-            "I work with founders from early ideation through validation & product development",
-            "I am often an early beta tester, active user, & paying customer of founders' products",
-            "I provide tactical support by making customer & candidate introductions from pre-product to post-IPO",
+            "We write the very first check into companies",
+            "We lead rounds with $1-2M checks and don't take board seats",
+            "We work with founders from early ideation through validation & product development",
+            "We are often an early beta tester, active user, & paying customer of founders' products",
+            "We provide tactical support by making customer & candidate introductions from pre-product to post-IPO",
           ].map((item, i) => (
             <div key={i} className="flex items-center space-x-3 text-sm">
               <span>* {item}</span>
@@ -110,9 +110,9 @@ export const About = () => {
           ))}
         </div>
         <div className="space-y-4">
-          <h2 className="font-semibold text-lg">Who I work with</h2>
+          <h2 className="font-semibold text-lg">Who We work with</h2>
           <div className="text-sm">
-            <p className="mb-4">I may be a good fit for you if:</p>
+            <p className="mb-4">We may be a good fit for you if:</p>
             <ul className="list-none space-y-2">
               <li>+ You&apos;ve built a company before and think may want to start another</li>
               <li>+ You love building and think you want to build something of your own someday</li>
@@ -120,7 +120,7 @@ export const About = () => {
             </ul>
           </div>
           <div className="text-sm">
-            <p className="mb-4">I may not be the best fit for you if:</p>
+            <p className="mb-4">We may not be the best fit for you if:</p>
             <ul className="list-none">
               <li>- You are already fundraising and we have never met before</li>
             </ul>
