b1f8041900b4d3c80caee085671dab7d6e5eab51
alanagoyal <alanaan@gmail.com>
2024-11-03 17:36:53 -0800

    site title

--- config/site.ts
+++ config/site.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
-    name: "Basecase_",
+    name: "Basecase dot sh",
     description: "A builder backing builders",
     url: process.env.NEXT_PUBLIC_URL || "",
     ogImage: process.env.NEXT_PUBLIC_OG_IMAGE || "",
