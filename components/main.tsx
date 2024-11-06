f91e0f91e191ed867644c11058790a38deee620b
alanagoyal <alanaan@gmail.com>
2024-11-06 09:19:02 -0800

    max-w

--- components/main.tsx
+++ components/main.tsx
@@ -9,7 +9,7 @@ export default function HomePage() {
   return (
     <div className="min-h-screen font-mono">
       <Navigation />
-      <div className="px-4 max-w-6xl mx-auto">
+      <div className="px-4 max-w-5xl mx-auto">
         <Content />
         <CTA />
       </div>
