a4fb0f269f83c0596a1d2672c4c676211a0f161b
alanagoyal <alanaan@gmail.com>
2024-11-09 10:43:23 -0800

    change animation to one word

--- components/hero.tsx
+++ components/hero.tsx
@@ -9,20 +9,19 @@ export const Hero = () => {
       <h1 className="text-3xl font-bold mb-4 flex flex-wrap cursor-default">
         {headingParts.map((part, index) => (
           <span key={index} className="whitespace-nowrap">
-            {/* Add line break before "future" on smaller screens */}
             {index === 3 && <span className="w-full block sm:hidden"></span>}
-            {/* Split each part into letters for hover effect */}
-            {part.split('').map((letter, letterIndex) => (
-              <span
-                key={letterIndex}
-                className="transition-colors duration-200 hover:text-[var(--color-primary)]"
-              >
-                {letter}
-              </span>
-            ))}
-            {/* Add space between words, except for the last word */}
-            {index < headingParts.length - 1 && (
-              <span>&nbsp;</span>
+            {index === 3 ? (
+              <>
+                <span className="transition-colors duration-200 hover:text-[var(--color-primary)]">
+                  future
+                </span>
+                <span>&nbsp;founders</span>
+              </>
+            ) : (
+              <>
+                {part}
+                {index < headingParts.length - 1 && <span>&nbsp;</span>}
+              </>
             )}
           </span>
         ))}
