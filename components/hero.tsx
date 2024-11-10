b6eb97668e84d17dfb8de14da891cff6f21941ab
alanagoyal <alanaan@gmail.com>
2024-11-10 14:15:26 -0800

    fix hero on mobile

--- components/hero.tsx
+++ components/hero.tsx
@@ -5,9 +5,12 @@ import { useScramble } from "use-scramble";
 export const Hero = () => {
   return (
     <div className="max-w-xl py-5">
-      <h1 className="text-3xl font-bold mb-4 cursor-default">
-        <span className="inline-block min-w-[24ch]">
-          <ScrambleText text="first check to future founders" />
+      <h1 className="text-3xl font-bold mb-4 cursor-default h-[80px] md:h-auto">
+        <span className="inline-block">
+          <ScrambleText 
+            text="first check to future founders" 
+            className="whitespace-pre-wrap md:whitespace-nowrap"
+          />
         </span>
       </h1>
       <p className="text-sm">
