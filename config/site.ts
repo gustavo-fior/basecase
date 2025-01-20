6b3765d02d8452b4e13017a8c23c3316b2708059
alanagoyal <alanaan@gmail.com>
2025-01-19 17:22:03 -0800

    change name

--- config/site.ts
+++ config/site.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
-  name: "basecase dot sh",
+  name: "basecase",
   description: "first check to future founders",
   url: process.env.NEXT_PUBLIC_URL || "",
   ogImage: process.env.NEXT_PUBLIC_OG_IMAGE || "",
